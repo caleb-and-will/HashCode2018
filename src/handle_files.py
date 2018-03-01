572110feadb98635211367677312419727881457
Caleb Hamilton <cjlhamilton@gmail.com>
2018-03-01 19:25:49 +0000

    Re-structured code

--- src/handle_files.py
+++ src/handle_files.py
@@ -8,25 +8,50 @@ class City:
 
     Properties:
         grid (int, int): (number of rows, number of columns)
-        vehicles_num: number of vehicles
+        vehicles (list of Vehicle): list of all available vehicles
+        rides (list of Ride): list of all rides
         ride_num: number of rides
         bonus: per-ride bonus for starting ride on time
         step_num: number of steps in the simulation
     """
-    def __init__(self, grid, vehicles_num, ride_num, bonus, step_num):
-        self.grid = grid
-        self.vehicles_num = vehicles_num
-        self.ride_num = ride_num
-        self.bonus = bonus
-        self.step_num = step_num
+    def __init__(self, file):
+        with open(file) as f:
+            line = f.readline()
+            values = line.split(' ')
+            values[-1] = values[-1][-2]
+        self.grid = (int(values[0]), int(values[1]))
+        self.ride_num = int(values[3])
+        self.bonus = int(values[4])
+        self.step_num = int(values[5])
+        self.vehicles = self.get_vehicles(int(values[2]))
+        self.rides = self.get_rides(file)
 
     def __repr__(self):
         return ('grid: ' + str(self.grid) +
-                '\nnumber of vehicles: ' + str(self.vehicles_num) +
+                '\nnumber of vehicles: ' + str(len(self.vehicles)) +
+                '\nvehicles: ' + str(self.vehicles) +
                 '\nnumber of rides: ' + str(self.ride_num) +
                 '\nper-ride bonus: ' + str(self.bonus) +
                 '\nnumber of steps: ' + str(self.step_num)
                 )
+
+    def get_rides(self, file):
+        rides = []
+        with open(file) as f:
+            next(f)
+            for line in f:
+                values = line.split(' ')
+                values[-1] = values[-1][-2]
+                r = Ride((int(values[0]), int(values[1])), (int(values[2]),
+                         int(values[3])), int(values[4]), int(values[5]))
+                rides.append(r)
+        return rides
+
+    def get_vehicles(self, n):
+        vehicles = []
+        for i in range(0, n):
+            vehicles.append(Vehicle((0, 0)))
+        return vehicles
 
 
 class Ride:
@@ -67,10 +92,11 @@ class Vehicle:
     def __init__(self, current_position):
         self.current_position = current_position
         self.ride = None
+        self.step_busy_until = 0
 
     def __repr__(self):
-        return ('current position: ' + str(self.current_position) +
-                '\nride: ' + str(self.ride)
+        return ('[' + str(self.current_position) +
+                ', ' + str(self.ride) + ']'
                 )
 
 
@@ -90,52 +116,11 @@ def create_matrix(r, c):
     return road_matrix
 
 
-def get_city(file):
-    """ Returns a City object given an input file.
-    """
-    with open(file) as f:
-        line = f.readline()
-        values = line.split(' ')
-        values[-1] = values[-1][-2]
-        c = City((int(values[0]), int(values[1])), int(values[2]),
-                 int(values[3]), int(values[4]), int(values[5]))
-        return c
-
-
-def get_rides(file):
-    """ Returns a list of Ride objects.
-    """
-    rides = []
-    with open(file) as f:
-        next(f)
-        for line in f:
-            values = line.split(' ')
-            values[-1] = values[-1][-2]
-            r = Ride((int(values[0]), int(values[1])), (int(values[2]),
-                     int(values[3])), int(values[4]), int(values[5]))
-            rides.append(r)
-    return rides
-
-
-def get_vehicles(city):
-    vehicles = []
-    for i in range(0, city.vehicles_num):
-        vehicles.append(Vehicle((0, 0)))
-    return vehicles
-
-
 def print_file_info(file):
-    city = get_city(file)
+    city = City(file)
     print(city)
 
     print('\n---\n')
 
-    vehicles = get_vehicles(city)
-    for v in vehicles:
-        print(v, '\n')
-
-    print('\n---\n')
-
-    rides = get_rides(file)
-    for r in rides:
+    for r in city.rides:
         print(r, '\n')
